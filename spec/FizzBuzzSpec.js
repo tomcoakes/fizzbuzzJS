7ade87c2cb38e86f51a6dff6cc6d2ff8ea7a7ba9
Yannick Brunner <yannickbrunner@gmail.com>
2015-03-03 12:30:48 +0000

    added play function, says FIZZ and BUZZ

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,32 +1,55 @@
 describe("FizzBuzz", function() {
+  
   var fizzbuzz;
 
   beforeEach(function() {
     fizzbuzz = new FizzBuzz();
   });
 
-  it("It know's when a number is divisible by three", function () {
-    expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
+  describe("Know's when a number is", function() {
+
+
+    it("divisible by three", function () {
+      expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
+    });
+
+    it("NOT divisible by three", function() {
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it("divisible by five", function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it("NOT divisible by five", function() {
+      expect(fizzbuzz.isDivisibleByFive(7)).toBe(false);
+    });
+
+    it("divisible by fifteen", function() {
+      expect(fizzbuzz.isDivisibleByFifteen(15)).not.toBe(false);
+    });
+
+    it("NOT divisible by fifteen", function() {
+      expect(fizzbuzz.isDivisibleByFifteen(16)).toBe(false);
+    });
+
+
   });
 
-  it("knows when a number is NOT divisible by three", function() {
-    expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+  describe("when playing, says", function() {
+
+
+    it("FIZZ, when a number is divisible by 3", function () {
+      expect(fizzbuzz.play(3)).toEqual("FIZZ");
+    });
+
+    it("BUZZ, when a number is divisible by 5", function () {
+      expect(fizzbuzz.play(5)).toEqual("BUZZ");
+    });
+
+
   });
 
-  it("knows when a number is divisible by five", function() {
-    expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
-  });
 
-  it("knows when a number is NOT divisible by five", function() {
-    expect(fizzbuzz.isDivisibleByFive(7)).toBe(false);
-  });
-
-  it("knows when a number is divisible by fifteen", function() {
-    expect(fizzbuzz.isDivisibleByFifteen(15)).not.toBe(false);
-  });
-
-  it("knows when a number is NOT divisible by fifteen", function() {
-    expect(fizzbuzz.isDivisibleByFifteen(16)).toBe(false);
-  });
 
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,9 +1,7 @@
 var FizzBuzz = function() {
 };
 
-FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
-  return (number % divisor === 0);
-};
+
 
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
   return this._isDivisibleBy(number, 3);
@@ -15,4 +13,15 @@ FizzBuzz.prototype.isDivisibleByFive = function(number) {
 
 FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
   return this._isDivisibleBy(number, 15);
+};
+
+FizzBuzz.prototype.play = function(number) {
+  return (this.isDivisibleByThree(number) ? "FIZZ" : "BUZZ");
+};
+
+
+//Private methods
+
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
 };
