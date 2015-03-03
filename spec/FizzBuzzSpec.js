cac2419d9a80725740c2c8b05d8c410a227b0cc1
Tom Coakes <tomcoakes@gmail.com>
2015-03-03 11:57:02 +0000

    Four tests passing, checking divisible by 3 or 5

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -3,11 +3,22 @@ describe("FizzBuzz", function() {
 
   beforeEach(function() {
     fizzbuzz = new FizzBuzz();
-  })
+  });
 
   it("It know's when a number is divisible by three", function () {
     expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
-  })
+  });
 
+  it("knows when a number is NOT divisible by three", function() {
+    expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+  });
 
-})
+  it("knows when a number is divisible by five", function() {
+    expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+  });
+
+  it("knows when a number is NOT divisible by five", function() {
+    expect(fizzbuzz.isDivisibleByFive(7)).toBe(false);
+  });
+
+});
